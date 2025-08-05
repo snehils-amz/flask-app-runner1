4cd74e699191825e37dd9a81f664d2928c21c47d
snehils-amz <snehils@amazon.com>
2025-08-05 12:50:14 +0530

    Add files via upload

---
+++ simple-flask/app.py
@@ -0,0 +1,14 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from AWS App Runner!'
+
+@app.route('/health')
+def health():
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
